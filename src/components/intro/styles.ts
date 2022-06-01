8fe4af33074f4cd46de439cb9d4ad61c3f6ad066
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-06-01 15:52:01 -0300

    test commit

--- src/components/intro/styles.ts
+++ src/components/intro/styles.ts
@@ -29,7 +29,7 @@ const Container = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 100%;
+    width: 100vw;
     position: absolute;
     top: 340px;
 
